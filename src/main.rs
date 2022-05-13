1f877efaf2581986f3ee450d4bcfbd9d8ff3cd47
Lily <lolpo12345@outlook.com>
2022-05-13 16:18:28 +0300

    Import necessary packages and add comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// We import the std::io library to read user input
+use std::io;
+// And then we import the package we just installed to generate random numbers
+// Under the hood, this library does a lot of low-level operations to generate true random numbers
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
 }
